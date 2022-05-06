af768e5f5394c3b8a8305ca90aff65e46ebbc6aa
cs <cs@cs.cs>
2022-05-06 08:37:48 +0200

    initial

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "aptos-fullnode" {
+  # download Terraform module from aptos-labs/aptos-core repo
+  source        = "github.com/aptos-labs/aptos-core.git//terraform/vultr/digital_ocean?ref=main"
+  region        = var.fullnode_region
+  era           = var.era
+  do_token      = var.api_key
+  image_tag     = var.image_tag
+  helm_values   = var.helm_values
+  fullnode_helm_values= var.fullnode_helm_values
+  fullnode_helm_values_list = var.fullnode_helm_values_list
+  helm_force_update = var.helm_force_update
+  k8s_namespace = var.k8s_namespace
+  k8s_api_sources = var.k8s_api_sources
+  num_fullnodes  = var.num_fullnodes
+  chain_id       = var.chain_id
+  machine_type   = var.machine_type
+}
+
+
